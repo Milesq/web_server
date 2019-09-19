5aecb3bd3c1d0884a90d8b50c1a0e5fb8d44778b
Milesq <wmilosz88@gmail.com>
2019-09-19 23:14:35 +0200

    Remove unneded comments from simple_server

--- examples/simple_server.rs
+++ examples/simple_server.rs
@@ -1,14 +1,6 @@
 extern crate http_server;
 
 fn main() {
-    // http_server::new()
-    //     .get("/ok/my/wer", |_, _| "ok2".into())
-    //     .get("/ok/:parameter", |req, _| {
-    //         format!("{:#?}", req.params).as_str().into()
-    //     })
-    //     .any("/ok/my/wer", |_, _| "any".into())
-    //     .launch(8080)
-    //     .unwrap();
     http_server::new()
         .get("/user/:id", |request, _| {
             format!("{:?}", request.params.get("id")).into()
